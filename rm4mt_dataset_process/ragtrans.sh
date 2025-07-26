739382b8684c6d2e8667d67e827d89843fb8d074
zihao <zihao-lee@outlook.com>
2025-07-26 17:52:16 +0300

    Add ragtrans.py and ragtrans.sh scripts for processing JSONL datasets; implement token counting and job submission configuration.

---
+++ rm4mt_dataset_process/ragtrans.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=ragtrans
+#SBATCH --output=/scratch/project_462000675/members/zihao/slurmlog/data_process/%x_%j.out
+#SBATCH --error=/scratch/project_462000675/members/zihao/slurmlog/data_process/%x_%j.err
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=32G
+#SBATCH --account=project_462000675
+
+start_time=$(date +%s)
+echo "Job started at: $(date)"
+
+module use /appl/local/csc/modulefiles/
+module load pytorch/2.5
+
+python ./ragtrans.py \
+    --input /scratch/project_462000941/members/zihao/rm4mt/rm4mt_dataset/raw/RAGtrans/RAGtrans_test.jsonl \
+    --output /scratch/project_462000941/members/zihao/rm4mt/rm4mt_dataset/processed/RAGtrans/en-zh.jsonl
+
+end_time=$(date +%s)
+echo "Job ended at: $(date)"
+
+duration=$((end_time - start_time))
+echo "Job duration: $(date -u -d @${duration} +%T)"
